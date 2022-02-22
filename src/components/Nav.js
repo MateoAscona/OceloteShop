f437aac38ba53d1814ce9d337f821bccf30d90e6
MateoAscona <asconamateo@gmail.com>
2022-02-22 20:18:25 -0300

    Desafio 2

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav className='nav'>
+        <li className='nav-item'><a href='' className='a__nav-item'>Buscar</a></li>
+        <li className='nav-item'><a href='' className='a__nav-item'>Carrito</a></li>
+        <li className='nav-item'><a href='' className='a__nav-item'>Login</a></li>
+    </nav>
+  )
+}
+
+export default Nav
